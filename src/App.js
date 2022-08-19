c727e7da9c4bb89fa9b4e725b6510f910ebd8bed
Sherydan <tobar.luis.c@gmail.com>
2022-08-19 03:58:52 -0400

    added bootstrap and basic card structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import Header from './components/Header';
-import Card from './components/Card';
+import CardComp from './components/Card';
 
 function App() {
   return (
     <div className="container">
       <Header title='Galeria de imagenes con React' />
-      <Card />
+      <CardComp imgSrc='default' title='Card title' description='text description' />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,21 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
+const CardComp = ({ imgSrc, title, description }) => {
+    return (
+        <>
+            <Card>
+                <Card.Img variant="top" src={imgSrc} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {description}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </>
+    );
+};
 
-const Card = ({imgSrc, title, description}) => {
-  return (
-    <>
-    
-    </>
-  )
-}
-
-export default Card
+export default CardComp
